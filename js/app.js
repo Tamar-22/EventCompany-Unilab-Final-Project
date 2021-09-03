b3fdce7f5b26305e530ad67ea2e05ec117ecc92c
Tamar-22 <tamar.bendeliani.2@ilauni.edu.ge>
2021-09-03 14:29:09 +0400

    change background video

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navlinks = document.querySelector (".nav-links");
-// const links = document.querySelector (".nav-links li");
 
-
-// hamburger.addEventListener("click", () => {
-//     navlinks.classList.toggle("open");
-// })
 
 var navbarLinks = document.getElementById("nav");
 var toggleButtonBurger = document.getElementById("toggleButton");
