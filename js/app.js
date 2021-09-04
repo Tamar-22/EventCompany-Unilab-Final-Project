20de243dc76106ba319842187a36025588bc1025
Tamar-22 <tamar.bendeliani.2@ilauni.edu.ge>
2021-09-04 14:37:46 +0400

    change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 var navbarLinks = document.getElementById("nav");
 var toggleButtonBurger = document.getElementById("toggleButton");
 
@@ -7,33 +5,12 @@ var toggleButtonBurger = document.getElementById("toggleButton");
 toggleButtonBurger.addEventListener("click", function(){
     navbarLinks.classList.toggle("active");
 })
-
-$(function(){
-    $('.single-item').slick({
-        arrows: false,
-        autoplay:true,
-        autoplayspeed: 3000,
-    });
-   
-});
-
   $(function() {
     $('.multiple-items').slick({
         infinite: true,
-        //რამდენი სურათი უნდა გამოჩნდეს ჩემს გვერდზე
         slidesToShow: 3,
-        //სლაიდის დროს რამდენი ელემნტით უნდა გადავიდეს ჩემი სლაიდერი
         slidesToScroll: 1,
-        //რომ ცამოჩნდეს ღილაკები (previuos & next)
         arrows: true,
-        // prevtArrow: false,
-        // nextArrow: false,
-        //რომ გამოჩნდეს ბურთულები
-        // dots:true,
-        // centerMode: true,
-        // pauseOnHover: true,
-        // autoplay:true,
-        // autoplayspeed: 2000,
         nextArrow: $(".next"),
         prevArrow: $(".prev"),
 
